e8ea55eac2f0a854755e21a5ebb5f1096bedc569
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-09 16:38:01 +0200

    struttura

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola Creare una funzione 
+// per capire se la parola inserita è palindroma
+
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri.
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Infine, dichiariamo chi ha vinto.
+
+// Consigli del giorno
+// Scriviamo sempre in italiano i passaggi che vogliamo fare
+// Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+
+
+var outputElement=document.getElementById('start');
+
+function checkPalindrome (str){
+    return str == str.split("").reverse().join("");
+}
+
+if(checkPalindrome(str) == askPrompt){
+ outputElement.innerHTML = 'The word which has been typed  by You is Palindrome';
+}
+else {
+    outputElement.innerHTML = 'The word which has been typed is Not Palindrome';
+}
+  var askPrompt = prompt('Inserire una parola');
+
+
+// function isPalindrome(str){
+	
+// 	if (str.length <= 1){
+//       outputElement.innerHTML= 'This is a palindrome';
+//     } 
+// 	else if (str.charAt(0) != str.slice(-1)){
+//     outputElement.innerHTML += 'This is not a palindrome';
+//     }
+// 	return isPalindrome(str.substring(1,str.length-1));
+// 	};
+
+
+
+
+
+    
+
+
+
+   
+
+
